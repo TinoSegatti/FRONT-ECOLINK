89a2b7239cc31ed8aff22e289cd56f04c1f140af
TinoSegatti <valentinosegatti@gmail.com>
2025-05-31 12:26:19 -0300

    Eliminar next.config.js y usar solo next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${formattedApiUrl}/api/v1/:path*`, // Cambiado a /api/v1
+        destination: `${formattedApiUrl}/api/v1/:path*`,
       },
     ];
   },
